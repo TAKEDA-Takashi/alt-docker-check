89fafd6169de0558ec0ff43cd8e036868975a4db
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2022-11-01 16:34:13 +0900

    Initial commit

---
+++ test/alt-docker-check.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AltDockerCheck from '../lib/alt-docker-check-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/alt-docker-check-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AltDockerCheck.AltDockerCheckStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
